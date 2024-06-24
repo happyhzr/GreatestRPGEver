7cf4bbd5f554084d66a403b6c92efc41279605be
happyhzr <insisthzr@163.com>
2024-06-24 16:52:30 +0800

    Add a player's movements

--- Assets/Scripts/MovementContoller.cs
+++ Assets/Scripts/MovementContoller.cs
@@ -7,24 +7,66 @@ public class MovementContoller : MonoBehaviour
     [SerializeField] private float movementSpeed = 3.0f;
 
     private Vector2 movement = new Vector2();
+    private Animator animator;
+    private string animationState = "AnimationState";
     Rigidbody2D rb2D;
+
+    private enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5,
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-
+        UpdateState();
     }
 
     private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
         rb2D.velocity = movement * movementSpeed;
+    }
+
+    private void UpdateState()
+    {
+        if (movement.x > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);
+        }
+        else if (movement.x < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+        }
     }
 }
