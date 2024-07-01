f189e9aa938230e81059633e1643b0edbc8c74af
happyhzr <insisthzr@163.com>
2024-07-01 21:26:05 +0800

    Add a scriptable object

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -8,7 +8,28 @@ public class Player : Character
     {
         if (collision.gameObject.CompareTag("CanBePickedUp"))
         {
-            collision.gameObject.SetActive(false);
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                Debug.Log("Hit: " + hitObject.name);
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+                collision.gameObject.SetActive(false);
+            }
         }
+    }
+
+    private void AdjustHitPoints(int amount)
+    {
+        hitPoints += amount;
+        Debug.Log("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
     }
 }
