6c47284f0adf9ee7ffd0925690ca832e519a7cdb
happyhzr <insisthzr@163.com>
2024-07-03 22:03:01 +0800

    Add a inventory

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Player : Character
 {
     [SerializeField] private HealthBar healthBarPrefab;
-
     private HealthBar healthBar;
+    [SerializeField] private Inventory inventoryPrefab;
+    private Inventory inventory;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,7 +21,7 @@ public class Player : Character
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
-                        shouldDisappear = true;
+                        shouldDisappear = inventory.AddItem(hitObject);
                         break;
                     case Item.ItemType.HEALTH:
                         shouldDisappear = AdjustHitPoints(hitObject.quantity);
@@ -38,6 +39,7 @@ public class Player : Character
 
     private void Start()
     {
+        inventory = Instantiate(inventoryPrefab);
         hitPoints.value = startingHitPoints;
         healthBar = Instantiate(healthBarPrefab);
         healthBar.character = this;
