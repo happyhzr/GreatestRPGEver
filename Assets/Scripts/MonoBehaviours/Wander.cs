67e6498f3127b2418a7e9a470cfea9914119ef21
happyhzr <insisthzr@163.com>
2024-07-10 22:04:13 +0800

    wander enimes

---
+++ Assets/Scripts/MonoBehaviours/Wander.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Animator))]
+public class Wander : MonoBehaviour
+{
+    [SerializeField] private float pursuitSpeed;
+    [SerializeField] private float wanderSpeed;
+    float currentSpeed;
+    [SerializeField] private float directionChangeInterval;
+    [SerializeField] private bool followPlayer;
+    private Coroutine moveCoroutine;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    private Transform targetTransform;
+    private Vector3 endPosition;
+    private float currentAngle;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentSpeed = wanderSpeed;
+        rb2d = GetComponent<Rigidbody2D>();
+        StartCoroutine(WanderRoutine());
+    }
+
+    private IEnumerator WanderRoutine()
+    {
+        while (true)
+        {
+            ChooseNewEndPoint();
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            moveCoroutine = StartCoroutine(Move(rb2d, currentSpeed));
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+    private void ChooseNewEndPoint()
+    {
+        currentAngle += Random.Range(0, 360);
+        currentAngle = Mathf.Repeat(currentAngle, 360);
+        endPosition += Vector3FromAngle(currentAngle);
+    }
+
+    private Vector3 Vector3FromAngle(float inputAngleDegrees)
+    {
+        float inputAngleRadians = inputAngleDegrees * Mathf.Deg2Rad;
+        return new Vector3(Mathf.Cos(inputAngleRadians), Mathf.Sin(inputAngleRadians), 0);
+    }
+
+    private IEnumerator Move(Rigidbody2D rigidBodyToMove, float speed)
+    {
+        float remainingDistance = (transform.position - endPosition).sqrMagnitude;
+        while (remainingDistance > float.Epsilon)
+        {
+            if (targetTransform != null)
+            {
+                endPosition = targetTransform.position;
+            }
+            if (rigidBodyToMove != null)
+            {
+                animator.SetBool("isWalking", true);
+                Vector3 newPosition = Vector3.MoveTowards(rigidBodyToMove.position, endPosition, speed * Time.deltaTime);
+                rb2d.MovePosition(newPosition);
+                remainingDistance = (transform.position - endPosition).sqrMagnitude;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+        animator.SetBool("isWalking", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && followPlayer)
+        {
+            currentSpeed = pursuitSpeed;
+            targetTransform = collision.gameObject.transform;
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            moveCoroutine = StartCoroutine(Move(rb2d, currentSpeed));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("isWalking", false);
+            currentSpeed = wanderSpeed;
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            targetTransform = null;
+        }
+    }
+}
